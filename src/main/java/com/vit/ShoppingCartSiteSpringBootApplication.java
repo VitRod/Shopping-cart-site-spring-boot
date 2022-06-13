60604497eefde690d4535c84ea3bce9d3c7b1a16
Vitaliy Rodyna <ukrdao@gmail.com>
2022-06-13 18:09:24 +0300

    first commit

---
+++ src/main/java/com/vit/ShoppingCartSiteSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.vit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartSiteSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartSiteSpringBootApplication.class, args);
+	}
+            
+}
---
+++ src/test/java/com/vit/ShoppingCartSiteSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartSiteSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
