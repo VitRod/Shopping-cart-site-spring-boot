2e35a3343d54a036d0a21ba8a65b09c85c0cc794
Vitaliy Rodyna <ukrdao@gmail.com>
2022-06-18 10:02:36 +0300

    added home controller and  simple home page

--- src/main/java/com/vit/controller/HomeController.java
+++ src/main/java/com/vit/controller/HomeController.java
@@ -1,8 +1,30 @@
 package com.vit.controller;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.vit.model.Product;
 
 @Controller
 public class HomeController {
+	
+	private static  final  int  INITIAL_PAGE = 0;
+	
+	@GetMapping("/home")
+	public  ModelAndView home(@RequestParam("page") Optional<Integer> page) {
+		
+		Product products = new Product();
+		
+		
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("products", products);
+		modelAndView.setViewName("/home");
+		return modelAndView;
+		
+	}
 
 }
