e90d2697d5085839b9677ca2902f24be8c6dfe11
Vitaliy Rodyna <ukrdao@gmail.com>
2022-06-22 16:37:38 +0300

    added ProductService interface

---
+++ src/main/java/com/vit/service/ProductService.java
@@ -0,0 +1,16 @@
+package com.vit.service;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.vit.model.Product;
+
+public interface ProductService {
+	
+	Optional<Product> findById(Long id);
+	
+	Page<Product> findAllProductsPageable(Pageable pageable);
+
+}
