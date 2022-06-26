61a9746504571bf946a3b88611a96844fd9ff2ac
Vitaliy Rodyna <ukrdao@gmail.com>
2022-06-26 03:13:49 +0300

    configured H2 in properties

--- src/main/java/com/vit/model/User.java
+++ src/main/java/com/vit/model/User.java
@@ -20,7 +20,7 @@ import org.hibernate.validator.constraints.NotEmpty;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Table(name="users")
+@Table(name="user")     
 public class User {
 
 	@Id
