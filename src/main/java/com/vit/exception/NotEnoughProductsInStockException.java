6f665819dd53dcbcb3c68acfa62bf536d7198a21
Vitaliy Rodyna <ukrdao@gmail.com>
2022-06-26 08:44:28 +0300

    small changes

--- src/main/java/com/vit/exception/NotEnoughProductsInStockException.java
+++ src/main/java/com/vit/exception/NotEnoughProductsInStockException.java
@@ -4,9 +4,6 @@ import com.vit.model.Product;
 
 public class NotEnoughProductsInStockException extends Exception {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	private static final String DEFAULT_MESSAGE = "Not enough products in stock";
@@ -19,5 +16,5 @@ public class NotEnoughProductsInStockException extends Exception {
         super(String.format("Not enough %s products in stock. Only %d left", product.getName(), product.getQuantity()));
     }
 
-}
+}                      
              
