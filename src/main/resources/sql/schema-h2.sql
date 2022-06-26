17fa6247cdb8be1f882dba285c419f2d46575852
Vitaliy Rodyna <ukrdao@gmail.com>
2022-06-26 14:46:55 +0300

    added  queries to the  Role and User_role tables

--- src/main/resources/sql/schema-h2.sql
+++ src/main/resources/sql/schema-h2.sql
@@ -15,3 +15,17 @@ VALUES (3, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'name
 INSERT INTO USER (user_id, password, email, username, name, last_name, active)
 VALUES (4, '$2a$06$OAPObzhRdRXBCbk7Hj/ot.jY3zPwR8n7/mfLtKIgTzdJa4.6TwsIm', 'vit@gmail.com', 'vituser', 'Vit',
         'Rodyna', 1);
+		
+INSERT INTO ROLE (role_id, role)
+VALUES (1, 'ROLE_ADMIN');
+INSERT INTO ROLE (role_id, role)
+VALUES (2, 'ROLE_USER');
+
+INSERT INTO USER_ROLE (user_id, role_id)
+VALUES (1, 1);
+INSERT INTO USER_ROLE (user_id, role_id)
+VALUES (1, 2);
+INSERT INTO USER_ROLE (user_id, role_id)
+VALUES (2, 2);
+INSERT INTO USER_ROLE (user_id, role_id)
+VALUES (3, 2);
