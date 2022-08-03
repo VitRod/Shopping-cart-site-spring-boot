118c639bdd023791fc8304d0c64da5e86638a503
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-03 08:17:32 +0300

    some code

--- src/main/java/com/vit/controller/LoginController.java
+++ src/main/java/com/vit/controller/LoginController.java
@@ -15,6 +15,6 @@ public class LoginController {
 			return "redirect:/home";    
 		}         
 		return "/login";                 
-	}                  
+	}                      
 }                        
                      
