26cce6eea6650e12662ecbeb8337691a21de24a8
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-04 07:29:34 +0300

    added code

--- src/main/java/com/vit/controller/CustomErrorController.java
+++ src/main/java/com/vit/controller/CustomErrorController.java
@@ -17,7 +17,7 @@ public class CustomErrorController implements ErrorController {
     }
 
     @GetMapping("/403")
-    public ModelAndView error403() {
+    public ModelAndView error403() {    
         return new ModelAndView("/403");
     }
 
