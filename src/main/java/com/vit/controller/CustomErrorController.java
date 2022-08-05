a9a089f0681c6fb3d9b6458d2a704aeadc8c01e1
Vitaliy Rodyna <ukrdao@gmail.com>
2022-08-05 13:30:47 +0300

    formatting

--- src/main/java/com/vit/controller/CustomErrorController.java
+++ src/main/java/com/vit/controller/CustomErrorController.java
@@ -23,7 +23,7 @@ public class CustomErrorController implements ErrorController {
                                                                               
     @Override                                  
     public String getErrorPath() {              
-        return PATH;
+        return PATH;    
     }                        
 }                                                           
           
